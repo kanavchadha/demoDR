13176f796e99a8a677b3affc4ea531f8ef537bb1
kanav chadha <kanavchadha56@gmail.com>
2021-07-10 16:18:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BasicForm from './components/BasicForm';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>learn react Forms</h1>
+      <BasicForm />
+    </div>
+  );
+}
+
+export default App;
